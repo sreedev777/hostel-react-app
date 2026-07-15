305a026361b15b3c780e54ac11e95d17f266f578
sreedev777 <sreedevkailas@gmail.com>
2026-07-15 13:02:59 +0530

    loginadd commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import AddStudent from './components/AddStudent'
 import Home from './components/home'
 import NavigationBar from './components/NavigationBar'
+import LoginAdd from './components/LoginAdd'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
     <Home/>
       <AddStudent/>
       <NavigationBar/>
+      <LoginAdd/>
     </>
   )
 }
